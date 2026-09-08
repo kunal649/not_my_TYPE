4be4e1f57d987e0a14bdeb2282f3a695a200bf91
kunal649 <kunalv672005@gmail.com>
2026-09-08 23:29:33 +0530

    Interface implementation with classes

--- index.ts
+++ index.ts
@@ -1,19 +1,23 @@
-
-let score = 67; // inference
+// inference
+let score = 67; 
 console.log(score); 
 
-function add(a: number, b: number) { // explicit annotation 
+
+// explicit annotation 
+function add(a: number, b: number) { 
     return a + b; 
 }
 let ans = add(6,7); 
 console.log(ans); 
 
 
-let arr: number[] = []; // explicit annotation 
+let arr: number[] = []; 
 arr.push(score, ans)
 console.log(arr)
  
-interface Num { // explicitly defining object's shape using "Interface"
+
+// Interface 
+interface Num { 
     id: number,
     type: string,
     isEven: boolean
@@ -29,3 +33,33 @@ function parseNumber() {
     console.log(num); 
 }
 parseNumber(); 
+
+
+// Interface declaration with classes 
+
+interface Land {
+    id : number;
+    name: string; 
+    location: any;
+    area: any;
+    isProfitable: boolean; 
+}
+
+class territory {
+    id : number;
+    name: string; 
+    location: any;
+    area: any; 
+    isProfitable: boolean; 
+
+    constructor(id: number, name:string, location, area, isProfitable:boolean) {
+        this.id = id;
+        this.name = name;
+        this.location = location;
+        this.area = area;
+        this.isProfitable = isProfitable; 
+    }
+}
+
+const land:Land = new territory(1, "DLF Promenade", "Phase 2,Gurugram-110056", "Gurugram NCR", true); 
+console.log(land); 
