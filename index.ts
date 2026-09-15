c484c6dffc012b2b4f5b6e5e0af324bfff605760
kunal649 <kunalv672005@gmail.com>
2026-09-15 23:19:21 +0530

    added: type narrowing - typeof, extends-constraint, utility type - Omit<T,K> & Pick<T,K>

--- index.ts
+++ index.ts
@@ -66,3 +66,46 @@ class territory {
 
 const land:Land = new territory(1, "DLF Promenade", "Phase 2,Gurugram-110056", "Gurugram NCR", true); 
 console.log(land); 
+
+
+// << ------------------------------------------Page End------------------------------------------------>>
+
+interface Product {
+  id: number;
+  name: string;
+  price: number;
+  category: string;
+}
+
+const product: Product = { 
+    id: 82, 
+    name: "Bazooka", 
+    price: 999, 
+    category: "defence", 
+}
+
+function toSummary(product: Product): Pick<Product, "id" | "name"> {
+    const profile = product as Pick<Product, "id" | "name">; 
+    return profile; 
+}
+function stripCategory(product: Product): Omit<Product, "category"> {
+  const profile = product as Omit<Product, "category">; 
+  return profile; 
+}
+
+// << ------------------------------------------Page End------------------------------------------------>>
+
+const settings = { theme: "dark", fontSize: 14 };
+type Settings = typeof settings;
+
+function updateSetting<K extends keyof Settings>(
+  settings: Settings,
+  key: K,
+  value: Settings[K]
+): Settings {
+    settings[key] = value; 
+    return settings; 
+}
+
+let ans1 = updateSetting(settings, "theme", "light"); 
+console.log(ans1); 
